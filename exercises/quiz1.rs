e678ff545f34e8074a500298608863bf2f83ae80
Cloud Chao <58268636@qq.com>
2023-08-26 06:57:02 +0000

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(v:u32) -> u32 {
+    if v>40 {
+        return v*1
+    }else{
+
+    }
+    v*2
+}
 
 // Don't modify this function!
 #[test]
